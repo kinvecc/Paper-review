fe0df9a006187e79df03856c4c45c53634d02a1f
Clrvn <1605509877@qq.com>
2020-03-16 11:28:17 +0800

    first commit

---
+++ src/main/java/com/clrvn/ClrvnApplication.java
@@ -0,0 +1,13 @@
+package com.clrvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClrvnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClrvnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clrvn/ClrvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clrvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClrvnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
